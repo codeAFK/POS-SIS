c469f9d61156084f3e27f1af13d07e3688691e1a
Gerald <grld4438@gmail.com>
2026-02-24 11:56:19 +0800

    Add files via upload

---
+++ POS-SIS/src/main/java/com/geraldcpe/possis/PosSisApplication.java
@@ -0,0 +1,13 @@
+package com.geraldcpe.possis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSisApplication.class, args);
+    }
+
+}
---
+++ POS-SIS/src/test/java/com/geraldcpe/possis/PosSisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldcpe.possis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosSisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
